6017790addbb9a6e517f37556a4cea676973cdfc
Brahima Traore <my123gate@gmail.com>
2017-07-13 11:20:58 -0400

    second commit

--- src/pack_1/Test_class.java
+++ src/pack_1/Test_class.java
@@ -10,14 +10,17 @@ public class Test_class {
 		System.out.println("date_of_birth = " + date_of_birth);
 		
 	}
-	static {
+	
+	public static void main(String[] args) {
+		getPersonInfo();
+	}
+static {
 		
 		System.out.println("The static content will be printed ealier");
 		//Let's assume we are in 2016 for this calculation
 		date_of_birth = 2016 - age; 
 	}
-	public static void main(String[] args) {
-		getPersonInfo();
-	}
 
+// The Static block gets computed before the main method. I could be used ti execute a program
+//prior to JDK7
 }
